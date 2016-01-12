173f896b26c9eaa4a85e2518ab3cd5873d6e1c76
dirtnail <dirtnail@gmail.com>
2016-01-12 17:10:02 +0100

    Gulp base

---
+++ gulp/config.js
@@ -0,0 +1,9 @@
+var src               = './app',
+    srcAssets         = src + '/_assets',
+    build             = './build',
+    development       = build + '/development',
+    developmentAssets = development + '/assets';
+    
+module.exports = {
+  
+};
