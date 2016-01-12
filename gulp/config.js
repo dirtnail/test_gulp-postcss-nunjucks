6f1e67db472709b098fd981000b4a58ad35bd9f0
dirtnail <dirtnail@gmail.com>
2016-01-12 17:17:32 +0100

    Browsersync and Watch

--- gulp/config.js
+++ gulp/config.js
@@ -5,5 +5,18 @@ var src               = './app',
     developmentAssets = development + '/assets';
     
 module.exports = {
-  
+  browsersync: {
+    development: {
+      server: {
+        baseDir: [development, build, src]
+      },
+      port: 9999,
+      files: [
+        developmentAssets + '/css/*.css',
+        developmentAssets + '/js/*.js',
+        developmentAssets + '/images/**',
+        developmentAssets + '/fonts/*'
+      ]
+    }
+  }  
 };
